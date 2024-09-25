3732534aed451477c57bd96c503cf65c12f39dd7
Adithya Krishna <aadithya794@gmail.com>
2024-09-25 12:59:23 +0530

    chore: added og image generation
    Signed-off-by: Adithya Krishna <aadithya794@gmail.com>

--- astro.config.ts
+++ astro.config.ts
@@ -8,46 +8,43 @@ import remarkUnwrapImages from "remark-unwrap-images";
 // @ts-ignore:next-line
 import { remarkReadingTime } from "./src/utils/remark-reading-time.mjs";
 
+import opengraphImages from "astro-opengraph-images";
+
 // https://astro.build/config
 export default defineConfig({
-	site: "https://adikris.in",
-	base: "/blog",
-	markdown: {
-		remarkPlugins: [remarkUnwrapImages, remarkReadingTime],
-		remarkRehype: { footnoteLabelProperties: { className: [""] } },
-		shikiConfig: {
-			theme: "dracula",
-			wrap: true,
-		},
-	},
-	integrations: [
-		mdx({}),
-		tailwind({
-			applyBaseStyles: false,
-		}),
-		sitemap(),
-		prefetch(),
-	],
-	vite: {
-		plugins: [rawFonts([".ttf"])],
-		optimizeDeps: {
-			exclude: ["@resvg/resvg-js"],
-		},
-	},
+    site: "https://adikris.in",
+    base: "/blog",
+    markdown: {
+        remarkPlugins: [remarkUnwrapImages, remarkReadingTime],
+        remarkRehype: { footnoteLabelProperties: { className: [""] } },
+        shikiConfig: {
+            theme: "dracula",
+            wrap: true,
+        },
+    },
+    integrations: [mdx({}), tailwind({
+        applyBaseStyles: false,
+		}), sitemap(), prefetch(), opengraphImages()],
+    vite: {
+        plugins: [rawFonts([".ttf"])],
+        optimizeDeps: {
+            exclude: ["@resvg/resvg-js"],
+        },
+    },
 });
 
 function rawFonts(ext: Array<string>) {
-	return {
-		name: "vite-plugin-raw-fonts",
-		// @ts-ignore:next-line
-		transform(_, id) {
-			if (ext.some((e) => id.endsWith(e))) {
-				const buffer = fs.readFileSync(id);
-				return {
-					code: `export default ${JSON.stringify(buffer)}`,
-					map: null,
-				};
-			}
-		},
-	};
+    return {
+        name: "vite-plugin-raw-fonts",
+        // @ts-ignore:next-line
+        transform(_, id) {
+            if (ext.some((e) => id.endsWith(e))) {
+                const buffer = fs.readFileSync(id);
+                return {
+                    code: `export default ${JSON.stringify(buffer)}`,
+                    map: null,
+                };
+            }
+        },
+    };
 }
